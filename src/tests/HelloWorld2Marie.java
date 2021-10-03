eae9059e8d95b87b7c98fe8e085dc5283f68eb3d
marie <marievictoriacharles@gmail.com>
2021-10-03 18:03:37 -0400

    updating hello world test

--- src/tests/HelloWorld2Marie.java
+++ src/tests/HelloWorld2Marie.java
@@ -4,6 +4,6 @@ public class HelloWorld2Marie {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-       
+        System.out.println("One more hello world");
     }
 }
