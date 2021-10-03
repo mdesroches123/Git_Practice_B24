cbdf26ee6cebabf94fa603ba3c9f14d449ef8932
marie <marievictoriacharles@gmail.com>
2021-10-03 17:50:52 -0400

    added wooden spoon test 2

---
+++ src/tests/WoodenSpoon2.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class WoodenSpoon2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Wooden Spoon");
+        }
+
+    }
+}
